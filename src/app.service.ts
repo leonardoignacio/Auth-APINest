25ef787a66e1a1798e35f4d4f1292c0df73a7492
leonardoignacio <leonardoignacio@hotmail.com>
2025-03-26 00:26:38 -0300

    ajuste para o build no railway e configuração de url padrão.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Exemplo de API hospedado no Railway, utilizando Nestjs + Prisma com banco de dados postgres publicado no SupaBase e sistema de autenticação com token JWT. ';
   }
 }
